1c30dd5f5340e29d6e47cf00fadb5c1b6bce4cf7
A.N <spidernet12@gmail.com>
2020-08-24 08:59:48 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const landing = require('../controllers/landing');
 
 router.get('/', landing.get_landing);
+router.post('/', landing.submit_lead);
 
 module.exports = router;

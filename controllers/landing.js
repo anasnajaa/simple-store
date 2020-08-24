975d79ef40995ed2d8ea0675f8398b0536077b62
A.N <spidernet12@gmail.com>
2020-08-24 10:23:29 +0300

    created landing page logic

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,3 +1,34 @@
+const leadModel = require('../models/leads')
+
 exports.get_landing = (req, res, next) => {
-    res.render('landing', { title: 'Express' });
+    res.render('landing', {
+        title: 'Express'
+    });
 };
+
+exports.submit_lead = (req, res, next) => {
+    const email = req.body.lead_email;
+
+    leadModel.add_email(email)
+        .then(db=>{
+            if(db.rowCount === 1) {
+                res.redirect("/");
+            } else {
+                res.redirect("/error");
+            }
+        })
+        .catch(error=>{
+            console.log(error);
+            res.redirect("/error");
+        });
+
+    const sendEmail = () => {
+        mailer.sendMail({
+            from: '"NoReply" <noreply@domain.com>',
+            to: email,
+            subject: "subject",
+            html: `message`
+        }, "email sent").catch(console.error);
+    };
+};
+

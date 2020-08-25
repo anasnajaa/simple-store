bfd246c9d779518448c95e5df85b8b1e2455d5c9
A.N <spidernet12@gmail.com>
2020-08-25 12:52:46 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ const router = express.Router();
 const landing = require('../controllers/landing');
 
 router.get('/', landing.get_landing);
-router.post('/', landing.submit_lead);
+
 router.get('/leads', landing.show_leads);
+router.post('/leads', landing.submit_lead);
+
 router.get('/lead/:id', landing.show_lead);
+
 router.get('/lead/:id/edit', landing.show_edit_lead);
 router.post('/lead/:id/edit', landing.edit_lead);
+
 router.post('/lead/:id/delete', landing.delete_lead);
 
 router.delete('/api/lead/:id', landing.api_delete_lead);

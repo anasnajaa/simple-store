3753bf91ede99ff4fe7a20f7f173bb3d1fcebfb4
A.N <spidernet12@gmail.com>
2020-08-26 08:24:55 +0300

    simplefied database url parsing

--- config/db-connect.js
+++ config/db-connect.js
@@ -1,15 +1,24 @@
 require('dotenv').config();
+const parsePostgressUrl = ()=>{
+  const dbUrl = process.env.DATABASE_URL;
+  const parsedDbUrl = dbUrl.split("//")[1].split(":");
+  const passwordAndDomain = parsedDbUrl[1].split("@");
+  const portAndDatabase = parsedDbUrl[2].split("/");
+  return {
+    host: passwordAndDomain[1],
+    user: parsedDbUrl[0],
+    password: passwordAndDomain[0],
+    database: portAndDatabase[1],
+    port: portAndDatabase[0],
+    max: 10,
+    idleTimeoutMillis: 30000,
+    ssl: {
+      rejectUnauthorized: false,
+    }
+  };
+};
+
 module.exports = {
     client: 'pg',
-    connection: {
-      host : process.env.DB_IP,
-      user : process.env.DB_USER,
-      password : process.env.DB_PASSWORD,
-      database : process.env.DB,
-      max: 10,
-      idleTimeoutMillis: 30000,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    }
+    connection: parsePostgressUrl()
 }

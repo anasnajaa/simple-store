9f11ab7ef522a516c91fdf091920814eb0f7521f
A.N <spidernet12@gmail.com>
2020-08-28 18:21:42 +0300

    refactored the route into admin and api

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const landing = require('../controllers/landing.controller');
+const index = require('../controllers/index.controller');
 const user = require('../controllers/user.controller');
 const {isLoggedIn, isAdmin, isLoggedOut} = require('../middleware/hasAuth');
 
-router.get('/', landing.get_landing);
-
-router.get('/leads', isAdmin, landing.show_leads);
-router.post('/leads', isAdmin, landing.submit_lead);
-
-router.get('/lead/:id', isAdmin, landing.show_lead);
-
-router.get('/lead/:id/edit', isAdmin, landing.show_edit_lead);
-router.post('/lead/:id/edit', isAdmin, landing.edit_lead);
-
-router.post('/lead/:id/delete', isAdmin, landing.delete_lead);
-
-router.delete('/api/lead/:id', isAdmin, landing.api_delete_lead);
+router.get('/', index.get_landing);
 
 router.get('/login', isLoggedOut, user.show_login);
 router.post('/login', isLoggedOut, user.login);
@@ -24,7 +12,7 @@ router.post('/login', isLoggedOut, user.login);
 router.get('/signup', isLoggedOut, user.show_signup);
 router.post('/signup', isLoggedOut, user.signup);
 
-router.get('/logout', isLoggedIn, user.logout);
-router.post('/logout', isLoggedIn, user.logout);
+router.get('/logout', user.logout);
+router.post('/logout', user.logout);
 
 module.exports = router;

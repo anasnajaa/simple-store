e756414ea3a5f84af20ba734da68bda878a967eb
A.N <spidernet12@gmail.com>
2020-08-29 21:01:09 +0300

    Update api.js

--- public/javascripts/api.js
+++ public/javascripts/api.js
@@ -14,4 +14,18 @@ const api = {
             } catch (error) { console.log(error); }
         }
     },
+    brand: {
+        deleteOne: async (id) => {
+            try {
+                const response = await fetch(`${api.endpoint}/brand/${id}`, {method: "DELETE"});
+                if(response.ok){
+                    let data = await response.json();
+                    if(data.deleted === 1) {
+                        document.getElementById("brand_"+id).remove();
+                        return true;
+                    }
+                }
+            } catch (error) { console.log(error); }
+        }
+    },
 };

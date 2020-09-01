8444521e87311a5e1b0eee76887f0e2622cf20f3
A.N <spidernet12@gmail.com>
2020-09-01 19:24:10 +0300

    Update brand.router.js

--- routes/brand.router.js
+++ routes/brand.router.js
@@ -4,12 +4,15 @@ const root = '/brands';
 
 exports.brandRoutes = (router)=>{
     router.get(root, isAdmin, brand.show_brands);
-    router.post(root, isAdmin, brand.submit_brand);
+    router.post(root, isAdmin, brand.show_brands);
     
     router.get(`${root}/:id`, isAdmin, brand.show_brand);
     
     router.get(`${root}/:id/edit`, isAdmin, brand.show_edit_brand);
     router.post(`${root}/:id/edit`, isAdmin, brand.edit_brand);
+
+    router.get(`${root}/add`, isAdmin, brand.show_add_brand);
+    router.post(`${root}/add`, isAdmin, brand.add_brand);
     
     router.post(`${root}/:id/delete`, isAdmin, brand.delete_brand);
 

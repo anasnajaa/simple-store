7c5b9fbc513477c196168bba0d638823ff2d7940
A.N <spidernet12@gmail.com>
2020-09-03 13:59:16 +0300

    added delete brand category route

--- routes/api.router.js
+++ routes/api.router.js
@@ -5,4 +5,6 @@ const brand = require('../controllers/brand.controller');
 
 router.delete('/brand/:id', isAdmin, brand.api_delete_brand);
 
+router.delete('/brand-category/:id', isAdmin, brand.api_delete_brand_category);
+
 module.exports = router;

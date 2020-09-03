6def00abd63eae6ca869992c65b13dbb8f152799
A.N <spidernet12@gmail.com>
2020-09-03 13:58:59 +0300

    added brand details, brand edit, brand categories routes

--- routes/brand.router.js
+++ routes/brand.router.js
@@ -1,20 +1,24 @@
 const { isAdmin } = require('../middleware/hasAuth');
 const brand = require('../controllers/brand.controller');
-const root = '/brands';
 
 exports.brandRoutes = (router)=>{
-    router.get(root, isAdmin, brand.show_brands);
-    router.post(root, isAdmin, brand.show_brands);
+    router.get('/brands', isAdmin, brand.show_brands);
+    router.post('/brands', isAdmin, brand.show_brands);
     
-    router.get(`${root}/:id`, isAdmin, brand.show_brand);
+    router.get(`/brands/:id`, isAdmin, brand.show_brand);
     
-    router.get(`${root}/:id/edit`, isAdmin, brand.show_edit_brand);
-    router.post(`${root}/:id/edit`, isAdmin, brand.edit_brand);
+    router.get(`/brands/:id/edit`, isAdmin, brand.show_edit_brand);
+    router.post(`/brands/:id/edit`, isAdmin, brand.edit_brand);
 
-    router.get(`${root}/add`, isAdmin, brand.show_add_brand);
-    router.post(`${root}/add`, isAdmin, brand.add_brand);
+    router.get(`/brands/add`, isAdmin, brand.show_add_brand);
+    router.post(`/brands/add`, isAdmin, brand.add_brand);
+
+    router.get(`/brands/:id/add-category`, isAdmin, (req, res, next)=>{
+        brand.show_add_brand_category(req, res, next, {}, {});
+    });
+    router.post(`/brands/:id/add-category`, isAdmin, brand.add_brand_category);
     
-    router.post(`${root}/:id/delete`, isAdmin, brand.delete_brand);
+    router.post(`/brands/:id/delete`, isAdmin, brand.delete_brand);
 
     return router;
 };

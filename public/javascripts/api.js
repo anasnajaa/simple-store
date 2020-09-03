5e72150365b6f08526d6e75965dfc43f62b7f170
A.N <spidernet12@gmail.com>
2020-09-03 14:01:15 +0300

    Update api.js
    added delete brand category logic

--- public/javascripts/api.js
+++ public/javascripts/api.js
@@ -26,6 +26,18 @@ const api = {
                     }
                 }
             } catch (error) { console.log(error); }
+        },
+        deleteCategory: async (brandCategoryId) => {
+            try {
+                const response = await fetch(`${api.endpoint}/brand-category/${brandCategoryId}`, {method: "DELETE"});
+                if(response.ok){
+                    let data = await response.json();
+                    if(data.deleted === 1) {
+                        document.getElementById("brand_category_"+brandCategoryId).remove();
+                        return true;
+                    }
+                }
+            } catch (error) { console.log(error); }
         }
     },
 };

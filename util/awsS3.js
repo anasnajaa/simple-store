0c52da63f4032963069d481227cb72845b0ab605
A.N <spidernet12@gmail.com>
2020-09-04 20:55:16 +0300

    added upload file and delete file methods for aws s3 helper class

--- util/awsS3.js
+++ util/awsS3.js
@@ -1,4 +1,7 @@
 const AWS = require('aws-sdk');
+const {
+    v4
+} = require('uuid');
 require('dotenv').config();
 
 const awsRegion = process.env.AWS_S3_REGION;
@@ -13,24 +16,74 @@ const s3 = new AWS.S3({
     secretAccessKey: awsAccessSecretKey
 });
 
-exports.uploadFile = (fileContent, fileName) => {
-    const params = {
-        Bucket: fileContent,
-        Key: fileName,
-        Body: fileContent
-    };
+exports.fileUrl = (fileName)=>{
+    return `https://${awsBucket}.s3.${awsRegion}.amazonaws.com/${fileName}`;
+}
 
-    s3.upload(params, (err, data) => {
-        if (err) {
-            console.log(err);
-            return null;
+exports.deleteFiles = (files)=> {
+    return new Promise((resolve)=>{
+        try {
+            const deleteParam = {
+                Bucket: awsBucket,
+                Delete: {Objects: []}
+            };
+        
+            files.forEach(file=>{
+                deleteParam.Delete.Objects.push({Key: file});
+            })
+        
+            s3.deleteObjects(deleteParam, (err, data) => {
+                if (err) {
+                    console.log(err);
+                    resolve(null);
+                } 
+                else {
+                    resolve(data);
+                }
+            });
+        } catch (error) {
+            console.log(error);
+            resolve(null);
         }
-        return data;
+    });
+}
+
+exports.uploadFile = (fileObject, fileName) => {
+    return new Promise((resolve) => {
+        try {
+            let fName = "";
+
+            if (fileName === null) {
+                fName = v4() + "." + fileObject.name.split(".")[1];
+            } else {
+                fName = fileName;
+            }
+
+            const fileContent = Buffer.from(fileObject.data, 'binary');
+
+            const params = {
+                Bucket: awsBucket,
+                Key: fName,
+                Body: fileContent
+            };
+
+            s3.upload(params, (err, data) => {
+                if (err) {
+                    console.log(err);
+                    resolve(null);
+                }
+                data.fileName = fName;
+                return resolve(data);
+            });
+        } catch (error) {
+            console.log(error);
+            resolve(null);
+        }
     });
 };
 
 exports.getSignedUrl = (fileName, fileType) => {
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject) => {
         const s3Params = {
             Bucket: awsBucket,
             Key: fileName,
@@ -38,7 +91,7 @@ exports.getSignedUrl = (fileName, fileType) => {
             ContentType: fileType,
             ACL: 'public-read'
         };
-    
+
         s3.getSignedUrl('putObject', s3Params, (err, data) => {
             if (err) {
                 console.log(err);
@@ -48,7 +101,7 @@ exports.getSignedUrl = (fileName, fileType) => {
                 fileName,
                 fileType,
                 signedRequest: data,
-                url: `https://${awsBucket}.s3.${awsRegion}.amazonaws.com/${fileName}`
+                url: this.fileUrl(fileName)
             });
         });
     });

d8fe93e112b9f4b57c172f7fcd5bf51bc7064cea
A.N <spidernet12@gmail.com>
2020-09-04 20:56:24 +0300

    Update brand_edit.js

--- public/javascripts/brand_edit.js
+++ public/javascripts/brand_edit.js
@@ -1,20 +1,20 @@
 util.listenForFileChanges("u_brand_thumb", async (file)=>{
     if(file){
-        const response = await api.s3.getS3SignedRequest(file);
-        if(response){
-            console.log(response);
-            const imgUrl = response.url;
-            const fileUploaded = await api.s3.uploadFile(file, response.signedRequest);
-            console.log(fileUploaded);
-            if(fileUploaded){
-                document.getElementById('brand_preview').src = imgUrl;
-                document.getElementById('thumbnail_url').value = imgUrl;
-            } else {
-                alert("Upload Failed");
-            }
-        } else {
-            alert("Upload Failed");
-        }
+        // const response = await api.s3.getS3SignedRequest(file);
+        // if(response){
+        //     console.log(response);
+        //     const imgUrl = response.url;
+        //     const fileUploaded = await api.s3.uploadFile(file, response.signedRequest);
+        //     console.log(fileUploaded);
+        //     if(fileUploaded){
+        //         document.getElementById('brand_preview').src = imgUrl;
+        //         document.getElementById('thumbnail_url').value = imgUrl;
+        //     } else {
+        //         alert("Upload Failed");
+        //     }
+        // } else {
+        //     alert("Upload Failed");
+        // }
     } else {
 
     }

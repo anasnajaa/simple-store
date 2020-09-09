ff45eaefe9a9203313e03e2e940a759e66c860b0
A.N <spidernet12@gmail.com>
2020-09-09 13:25:21 +0300

    initial area controller

---
+++ controllers/area.controller.js
@@ -0,0 +1,25 @@
+
+
+exports.show_areas = (req, res, next)=>{
+
+}
+
+exports.show_edit_area = (req, res, next, formData, errors)=>{
+    
+}
+
+exports.edit_area = (req, res, next)=>{
+    
+}
+
+exports.show_add_area = (req, res, next)=>{
+    
+}
+
+exports.add_area = (req, res, next)=>{
+    
+}
+
+exports.delete_area = (req, res, next)=>{
+    
+}

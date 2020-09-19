f84ac53bff3c2009749f8db1af9503e51995cca7
A.N <spidernet12@gmail.com>
2020-09-19 12:24:01 +0300

    seperated between website login and jwt login

--- routes/main.router.js
+++ routes/main.router.js
@@ -7,12 +7,12 @@ const {isLoggedIn, isAdmin, isLoggedOut} = require('../middleware/hasAuth');
 router.get('/', index.get_landing);
 
 router.get('/login', isLoggedOut, user.show_login);
-router.post('/login', isLoggedOut, user.login);
+router.post('/login', isLoggedOut, user.admin_login);
 
 router.get('/signup', isLoggedOut, user.show_signup);
-router.post('/signup', isLoggedOut, user.signup);
+router.post('/signup', isLoggedOut, user.admin_signup);
 
-router.get('/logout', user.logout);
-router.post('/logout', user.logout);
+router.get('/logout', user.admin_logout);
+router.post('/logout', user.admin_logout);
 
 module.exports = router;

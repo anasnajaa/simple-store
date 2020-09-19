08235f96e9d801c22f426ec3b827b9ecb37dea5c
A.N <spidernet12@gmail.com>
2020-09-19 12:21:51 +0300

    added jwt, cors, i18n, and mongoose support

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
+const cors = require('cors');
+const i18n = require('i18n');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
@@ -15,29 +18,55 @@ const routes = require('./routes/index.router');
 
 require('./util/passport_setup')(passport);
 
+const environment = process.env.NODE_ENV;
+const stage = require('./config/index')[environment];
+
+// mongo db
+mongoose.connect(stage.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+connection.once('open', () => console.log("MongoDB connection established"));
+
+// server config
+
 const app = express();
+
+i18n.configure({
+  locales: ['en', 'ar'],
+  cookie: 'lang',
+  queryParameter: 'lang',
+  directory: './locales'
+});
+
+app.disable('x-powered-by'); 
+
+app.use(cors(stage.corsOptions));
 app.use(require('express-status-monitor')());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(i18n.init);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
 
 app.use(session({
-  secret: process.env.SESSION_SECRET, 
+  secret: stage.sessionSecret, 
   resave: false, 
   saveUninitialized: false,
   store: new MongoStore({
-    url: process.env.ATLAS_URI_RW
+    url: stage.mongoUri
   }),
   cookie: {
     maxAge: 1 * 5 * 60 * 1000 // 5 minutes
   }
 }));
+
+if (environment !== 'production') {
+    app.use(logger('dev'));
+}
 
 app.use(connectFlash());
 
@@ -58,14 +87,6 @@ app.use((req, res, next) => {
   next(createError(404));
 });
 
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV === 'development' ? err : {};
-
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-app.listen(process.env.PORT || '80', () => {
-  console.log('Server started on: ' + process.env.PORT);
+app.listen(stage.port || '80', () => {
+  console.log('Server started on: ' + stage.port);
 });

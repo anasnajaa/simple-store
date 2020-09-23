97bb98a09ba8b8e513b24987d0fb332045dc29d3
A.N <spidernet12@gmail.com>
2020-09-23 15:47:27 +0300

    initial error handler method logic

--- util/errorHandler.js
+++ util/errorHandler.js
@@ -1,3 +1,13 @@
-exports.logError = (error)=> {
-    console.log(error);
+require('dotenv').config();
+const errorEmail = require('../email/apiErrorEmail');
+const environment = process.env.NODE_ENV;
+
+exports.apiError = (res, error)=> {
+    if (environment !== 'production') {
+        res.status(500).json({status: -1, error: error.message});
+        console.log(error);
+    } else {
+        res.status(500).json({status: -1, error: "Server Error, please contact support"});
+        errorEmail.sendEmail(error);
+    }
 }

1c823b1863de3ce37fd1ad28d09f2b60ca7e9e9f
A.N <spidernet12@gmail.com>
2020-09-28 08:04:52 +0300

    wraped the sendMessage method in a promise

--- util/sms.js
+++ util/sms.js
@@ -4,18 +4,21 @@ const stage = require('../config/index')[environment];
 const client = require('twilio')(stage.sms.accountSid, stage.sms.authToken);
 const smsModel = require('../models/sms.m');
 
-exports.sendMessage = async (to, body)=> {
-    try {
-        const response = await client.messages
-        .create({
-            from: stage.sms.fromNumber,
-            statusCallback: stage.publicUrl + '/api/update-sms-status',
-            body,
-            to
-         });
-         const newRecord = await new smsModel(response).save();
-         console.log("Message ok", response, newRecord);
-    } catch (error) {
-        console.log("Message failed", error);
-    }
+exports.sendMessage =  (to, body)=> {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const response = await client.messages
+            .create({
+                from: stage.sms.fromNumber,
+                statusCallback: stage.publicUrl + '/api/update-sms-status',
+                body,
+                to
+             });
+             const newRecord = await new smsModel(response).save();
+             resolve({serviceResponse: response, addedRecord: newRecord});
+        } catch (error) {
+            console.log("Message failed", error);
+            reject(error);
+        }
+    });
 }

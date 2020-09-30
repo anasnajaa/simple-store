2975c86fb7cc1b801f73eb128f50f4dcdc6793e6
A.N <spidernet12@gmail.com>
2020-09-30 14:12:12 +0300

    Create token.m.js
    semilar to session implementation of JWT that provides ease of access, speed and control

---
+++ models/token.m.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    token: { type: String, required: false },
+    user: { type: Object, required: false },
+    lastActive: { type: Date, required: false },
+    lastIp: { type: Date, required: false },
+    lastUserAgent: { type: Object, required: false },
+    isExpired: { type: Boolean, required: false },
+}, 
+{ timestamps: true});
+
+const Tokens = mongoose.model('Tokens', schema);
+
+module.exports = Tokens; 

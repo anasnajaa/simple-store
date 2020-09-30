4ba76f3791094457e4b1adb48b61a5acc402cc5a
A.N <spidernet12@gmail.com>
2020-09-30 08:21:39 +0300

    initial database error handling

--- util/errorHandler.js
+++ util/errorHandler.js
@@ -1,13 +1,22 @@
 require('dotenv').config();
-const errorEmail = require('../email/apiErrorEmail');
+const apiMail = require('../email/apiErrorEmail');
+const dbMail = require('../email/dbErrorEmail');
 const environment = process.env.NODE_ENV;
 
-exports.apiError = (res, error)=> {
+exports.apiError = (res, error) => {
     if (environment !== 'production') {
         res.status(500).json({status: -1, error: error.message});
         console.log(error);
     } else {
         res.status(500).json({status: -1, error: "Server Error, please contact support"});
-        errorEmail.sendEmail(error);
+        apiMail.sendEmail(error);
+    }
+}
+
+exports.databaseError = (error) => {
+    if (environment !== 'production') {
+        console.log(error);
+    } else {
+        dbMail.sendEmail(error);
     }
 }

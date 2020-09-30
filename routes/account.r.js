c2fd98a50c23085d7886d9915046d27ebc8e4021
A.N <spidernet12@gmail.com>
2020-09-30 15:04:45 +0300

    Update account.r.js

--- routes/account.r.js
+++ routes/account.r.js
@@ -2,8 +2,8 @@ const { isLoggedIn, isLoggedOut } = require('../middleware/hasAuth');
 const user = require('../controllers/user.c');
 
 exports.init = (router)=>{
-    router.post('/account/login', isLoggedOut, user.login);
-    router.post('/account/logout', isLoggedIn, user.logout);
+    router.post('/account/login', user.login);
+    router.post('/account/logout', user.logout);
     router.get('/account/profile', isLoggedIn, user.profile);
     router.post('/account/register', user.register);
     router.get('/account/activate', user.activateAccount);
